53b8a1a09310874298815be0c87bddb38c65d645
Phumlani Arendse <aphumlani.dev@gmail.com>
2021-12-03 01:20:17 +0200

    Make it better

--- mongoDB/db.js
+++ mongoDB/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 // connect to mongoDB
-const connect = mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+const connect = mongoose.connect(process.env.DB_URL);
 
 // database structure aka schema
 const postSchema = {
