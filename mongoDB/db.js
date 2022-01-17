efe189468f00794edf23889403e2ff50a3a4404c
Phumlani Arendse <aphumlani.dev@gmail.com>
2022-01-18 01:36:28 +0200

    connected to mongoDB atlas

--- mongoDB/db.js
+++ mongoDB/db.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
 
 // connect to mongoDB
-const connect = mongoose.connect(process.env.DB_URL);
+const connect = mongoose.connect(
+	`mongodb+srv://admin-phumlani:${process.env.DB_PSWD}@cluster0.jt1kf.mongodb.net/blog?retryWrites=true&w=majority`,
+	{ useNewUrlParser: true }
+);
 
 // database structure aka schema
 const postSchema = {
